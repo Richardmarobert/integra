37bdb66ea161777e76f7f40410d51a1bc544bbf2
Richard Robert <richard.robert@hotmail.nl>
2018-11-10 20:17:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 
 var Service,
     Charasteristic,
-    Integra_ip,
-    Integra_port,
-    Integra_user;
-    
+    UUIDGen;
+
 module.exports = function (homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
+	UUIDGen = homebridge.hap.uuid;
+
 	homebridge.registerPlatform("homebridge-integra", "Integra", Integra);
   
-  
+}
+
+Integra.prototype = {
+	getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "Satel")
+      .setCharacteristic(Characteristic.Model, "Integra 32")
+      .setCharacteristic(Characteristic.Usercode, "1102");
+		
+ 
+}
